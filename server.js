644a87f3f906314a7a76992d3d1391ca66fdb68f
leysont <leysont@tutanota.com>
2026-02-14 15:53:15 +0100

    update server port to 5173

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 const path = require("path")
 const { exec } = require("child_process")
 
-const PORT = 3000
+const PORT = 5173
 const DIST = path.join(__dirname, "dist")
 
 const MIME_TYPES = {
